a86c0f3c29368da38cb54c7aa3539370429ad416
precaf <precaffeinated@icloud.com>
2022-01-04 15:26:16 -0500

    Initial load

---
+++ ios/scripts/adjust.js
@@ -0,0 +1,17 @@
+window.addEventListener("orientationchange", function() {
+	whatsWhat();
+	window.location.reload();
+}, false);
+
+function whatsWhat () {
+	var squares = 77;
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	var total = width * height;
+	var pixels = screen.pixelDepth;
+	var avg = Math.sqrt(total/squares)-25;
+	var psize = Math.floor(avg);
+	document.write('\n<style>img { width:' + psize + 'px; height:' + psize + 'px; }</style>\n');
+}
+
+whatsWhat();
