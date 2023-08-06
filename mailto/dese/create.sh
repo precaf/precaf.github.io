78ba4c129b6b0cf4b5c8e8f85725646594c05e70
precaf <97126718+precaf@users.noreply.github.com>
2023-08-06 13:39:56 -0400

    dese2

--- mailto/dese/create.sh
+++ mailto/dese/create.sh
@@ -15,13 +15,13 @@ Malden, MA 02148
 
 Dear Ms. McKinnon:
 
-I am writing today in full support of the new comprehensive health and physical education curriculum framework. An updated framework grounded in science and reflecting best practices helps health educators teach students what they need to know at every grade level. 
+I am writing today in full support of the new comprehensive health and physical education curriculum framework. An updated framework grounded in science that reflects best practices helps health educators teach students what they need to know at every grade level. 
 
-According to SIECUS, which follows state trends in sex education, Massachusetts is one of 21 states that does not (yet) require sex education and our standards covering STDs and health relationships need some work. Federal legislation like Barbara Lee's Real Education and Access for Healthy Youth Act of 2023, Governor Healy's proposals which have been expanded in the DESE draft, and the Massachusetts Act Relative to Healthy Youth all recognize this deficit. The state legislation is endorsed by the Massachusetts Healthy Youth Coalition and it offers an opt-out for parents whose religious beliefs do not permit them to accept non-normative aspects of sexuality and gender.
+Massachusetts is one of 21 states that does not yet require sex education and existing standards covering STDs and health relationships still need work. Governor Healy's proposals which have been realized in the DESE draft and supported by new state legislation recognize and address these deficits. They also address concerns of parents whose religious beliefs do not accept non-normative aspects of sexuality and gender since the legislation provides for opting-out.
 
-Nevertheless, it is important that students have a health curriculum that emphasizes the importance of consent in a healthy relationship and one that is inclusive of students of all gender identities and sexual orientations. 
+But for an overwhelming number of Commonwealth families it is important that students have a health curriculum which emphasizes the importance of consent in a healthy relationship and one that is inclusive of students of all gender identities and sexual orientations. 
 
-I urge BESE to vote to adopt this new Framework and look forward to learning how DESE will support educators to implement it in their classrooms. 
+I urge BESE to vote to adopt this new Framework and I look forward to learning how DESE will support educators in implementing it in their classrooms. 
 
 Sincerely,
 
