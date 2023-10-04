72d8e093f2f161828ac8f75b3516a169fdeee160
precaf <97126718+precaf@users.noreply.github.com>
2023-10-04 14:41:08 -0400

    can't get it

--- others/ios2/scripts/adjust.js
+++ others/ios2/scripts/adjust.js
@@ -6,16 +6,15 @@ window.addEventListener("orientationchange", function() {
 function whatsWhat () {
 	var width = window.innerWidth;
 	var height = window.innerHeight;
-	var dim1 = 1; var dim2 = 1; psize = 1;
+	var dim1 = 1; var dim2 = 1;
 	if (width < height) {
 	   dim1 = Math.floor((width-108)/8);
 	   dim2 = Math.floor((height-144)/11);
-	   psize = Math.min(dim1,dim2);
 	} else {
 	   dim1 = Math.floor((height-108)/8);
 	   dim2 = Math.floor((width-144)/11);
-	   var psize = Math.max(dim1,dim2);
 	}	
+	var psize = Math.max(dim1,dim2);
 	var DEBUG = 'width='+width+' height='+height+' dim1='+dim1+' dim2='+dim2+' psize='+psize
 	document.write('\n<style>img { width:' + psize + 'px; height:' + psize + 'px; }</style>\n');
 	document.write('\n<!--  '+DEBUG+'  -->\n');
