8d6b6b4b4b46a84ee21251cba9919d85a6726292
precaf <97126718+precaf@users.noreply.github.com>
2023-10-04 15:48:26 -0400

    fd

--- others/ios2/scripts/adjust.js
+++ others/ios2/scripts/adjust.js
@@ -4,6 +4,8 @@ window.addEventListener("orientationchange", function() {
 }, false);
 
 function whatsWhat () {
+    var userAgent = navigator.userAgent;
+    var browserName = 'Desktop';
 	var width = window.innerWidth;
 	var height = window.innerHeight;
 	var pixwidth = 1; var pixheight = 1; var psize = 1;
@@ -16,7 +18,14 @@ function whatsWhat () {
 	   pixheight = Math.floor(width/8)-14;
 	   psize = Math.max(pixwidth,pixheight);
 	}	
-	var DEBUG = 'width='+width+' height='+height+' pixwidth='+pixwidth+' pixheight='+pixheight+' psize='+psize
+	if (navigator.userAgent.match(/iPad/i)) {
+	   browserName = 'iPad';
+	} else {
+	   if (navigator.userAgent.match(/iPad/i)) {
+	      browserName = 'iPhone';
+	   }
+	}
+	var DEBUG = 'browserName='+browserName+' width='+width+' height='+height+' pixwidth='+pixwidth+' pixheight='+pixheight+' psize='+psize
 	document.write('\n<style>img { width:' + psize + 'px; height:' + psize + 'px; }</style>\n');
 	document.write('\n<!--  '+DEBUG+'  -->\n');
 }
