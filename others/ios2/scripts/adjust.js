9a99374313587766e78e41b75a54cf25b7adc172
precaf <97126718+precaf@users.noreply.github.com>
2023-10-04 14:07:31 -0400

    once more

--- others/ios2/scripts/adjust.js
+++ others/ios2/scripts/adjust.js
@@ -14,7 +14,7 @@ function whatsWhat () {
 	   dim1 = Math.floor((height-108)/8);
 	   dim2 = Math.floor((width-144)/11);
 	}
-	var psize = Math.max(dim1,dim2);
+	var psize = Math.min(dim1,dim2);
 	var DEBUG = 'width='+width+' height='+height+' dim1='+dim1+' dim2='+dim2+' psize='+psize
 	document.write('\n<style>img { width:' + psize + 'px; height:' + psize + 'px; }</style>\n');
 	document.write('\n<!--  '+DEBUG+'  -->\n');
