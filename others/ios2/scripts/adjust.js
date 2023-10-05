99d78f66e9979c777beeb4ea0f1df23c29d674b6
precaf <97126718+precaf@users.noreply.github.com>
2023-10-05 18:25:53 -0400

    Update adjust.js

--- others/ios2/scripts/adjust.js
+++ others/ios2/scripts/adjust.js
@@ -23,18 +23,20 @@ function whatsWhat () {
 	var browserwidth = window.innerWidth;
 	var browserheight = window.innerHeight;
 	var landscape = true;
+	var x = 1; var y = 1;
 	if (browserwidth < browserheight) {
 	  landscape = false;
-	}
-	if (landscape) {
-	   psize = Math.floor((browserwidth-12*12)/11)
+	  x = Math.floor(browserwidth/8);
+	  y = Math.floor(browserheight/11);
 	} else {
-	   psize = Math.floor((browserwidth-9*12)/8)
+	  x = Math.floor(browserwidth/11);
+	  y = Math.floor(browserheight/8);
 	}
+	var psize = Math.min(x,y);
 	
-	var DEBUG = 'userAgent='+userAgent+' browserName='+browserName+' width='+width+' height='+height+' browserwidth='+browserwidth+' browserheight='+browserheight+' landscape='+landscape+' psize='+psize
+	var DEBUG = 'userAgent='+userAgent+' browserName='+browserName+' width='+width+' height='+height+' browserwidth='+browserwidth+' browserheight='+browserheight+' landscape='+landscape+' psize='+psize+' x='+x+' y='+y;
 	document.write('\n<style>img { width:' + psize + 'px; height:' + psize + 'px; }</style>\n');
-	document.write('\n<!--  '+DEBUG+'  -->\n');
+	document.write('\n<!-- '+DEBUG+' -->\n');
 }
 
 whatsWhat();
