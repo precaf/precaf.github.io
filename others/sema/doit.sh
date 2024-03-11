746bb62177558e254a2985808873e7b28c7b9c23
precaf <97126718+precaf@users.noreply.github.com>
2024-03-11 13:27:29 -0400

    sema

---
+++ others/sema/doit.sh
@@ -0,0 +1,23 @@
+function process {
+  echo "<a href=\"$2\" target=\"_blank\"><img alt=\"$1\" title=\"$1\" src=\"icons/$3\"></a>"
+}
+
+echo "<br/>"
+echo "<br/>"
+echo "<br/>"
+
+process "Linktree" "https://linktr.ee/sema.freepalestine" "linktree.png"
+process "Join us" "https://tiny.cc/sema-activist" "joinus.png"
+process "Help!" "https://semafreepalestine.vivaldi.net" "Help.png"
+
+echo "<br/>"
+
+process "Substack" "https://ceasefirenow.substack.com" "Substack.png"
+process "Calendar" "https://tiny.cc/sema-calendar" "Calendar.png"
+process "Add Calendar Event" "https://tiny.cc/sema-addevent" "AddEvent.png"
+
+echo "<br/>"
+
+process "Instagram" "https://www.instagram.com/sema.freepalestine/" "Instagram.png"
+process "Mastodon" "https://social.vivaldi.net/@semafreepalestine" "Mastodon.png"
+process "Threads" "https://www.threads.net/@sema.freepalestine" "Threads.png"
